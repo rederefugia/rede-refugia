da9458e67912cfca4fde785ef346860001b2941d
Bruno Lima Cardoso <blimacardoso@gmail.com>
2022-03-04 15:25:57 -0300

    it redesign the login screen using react native paper components

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-import localization from "../../utils/localization";
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { Title, TextInput, Button, HelperText, Card } from "react-native-paper";
 
 /**
  * @memberof Screens
@@ -9,11 +8,43 @@ import localization from "../../utils/localization";
  * @description It implemets the Login Screen page
  */
 const LoginScreen = () => {
-    return (
-        <View style={styles.view}>
-            <Text>{localization.t('login')}</Text>
-        </View>
-    );
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const hasErrors = () => {
+    return username.length > 0 && !username.includes("@");
+  };
+
+  return (
+    <View style={styles.view}>
+      <Card>
+        <Card.Title title="Login" />
+        <Card.Content>
+          <TextInput
+            placeholder="Email"
+            value={username}
+            onChangeText={setUsername}
+            left={<TextInput.Icon name="account" />}
+          />
+          <HelperText type="error" visible={hasErrors()}>
+            Email address is invalid!
+          </HelperText>
+          <TextInput
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            left={<TextInput.Icon name="key-variant" />}
+          />
+        </Card.Content>
+        <Card.Actions style={styles.actions} >
+          <Button icon="login-variant" mode="contained">
+            Enter
+          </Button>
+        </Card.Actions>
+      </Card>
+    </View>
+  );
 };
 
 /**
@@ -21,7 +52,8 @@ const LoginScreen = () => {
  * @description it implements the Login screen page' style grouped by component view
  */
 const styles = StyleSheet.create({
-    view: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  view: { flex: 1, alignItems: "center", justifyContent: "center" },
+  actions: { paddingHorizontal: "16px" }
 });
 
 export default LoginScreen;
