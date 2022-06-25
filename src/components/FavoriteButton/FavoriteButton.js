46cb201899c4aa611ef8c0f64897abe34ac2e669
Bruno Lima Cardoso <blimacardoso@gmail.com>
2022-06-24 23:02:07 -0300

    it creates a method to add an opportunity as an user's favorite

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -5,10 +5,25 @@ import { IconButton } from "react-native-paper";
 
 import providers from "../../providers";
 import theme from "../../utils/theme";
+import firestore from "../../utils/firebase/firestore";
 
 const FavoriteButton = ({ id }) => {
   const { user, setUser } = React.useContext(providers.auth.AuthContext);
   const [isFavorite, setIsFavorite] = React.useState(false);
+
+  const addFavorite = async () => {
+    let favorites = user.favorites ? user.favorites : [];
+    favorites.push(id);
+    await firestore.updateById(firestore.COLLECTIONS.USERS, user.uid, {
+      favorites,
+    });
+    await setUser({ ...user, favorites });
+    setIsFavorite(true);
+  };
+
+  const update = async () => {
+    if (!isFavorite) await addFavorite();
+  };
 
   React.useEffect(() => {
     if (user.favorites)
@@ -22,6 +37,7 @@ const FavoriteButton = ({ id }) => {
       icon={isFavorite ? "heart" : "heart-outline"}
       color={theme.DefaultTheme.colors.purple}
       style={styles.icon}
+      onPress={update}
     />
   );
 };
